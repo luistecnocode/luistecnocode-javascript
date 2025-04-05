593f9144ae3c89d13a2192de8f502f372cff6fb3
luistecnocode <luistecnocode@gmail.com>
2025-04-05 18:52:19 +0200

    Primeros ejercicios de Javascript basico de Mouredev

---
+++ Basic/01-variables.js
@@ -0,0 +1,25 @@
+// var
+// NO DEBE UTILZARSE ASÍ
+// Es accesible desde cualquier sitio
+var helloWorld = "¡Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!" 
+console.log(helloWorld)
+
+// let
+// SI DEBE SER ASÍ.
+// Solo son accesibles dentro de un bloque
+// Si accedemos antes de haberla declarado dará error
+
+let helloWorld2 = "¡Hola, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3= "¡Hola, JavaScript 3!" 
+// No puede ser reasignada
+console.log(helloWorld3)
